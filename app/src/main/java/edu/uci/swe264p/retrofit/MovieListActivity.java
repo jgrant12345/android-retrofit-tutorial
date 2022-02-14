4ab64943532715656fd462cf77e97731e89a5892
Justin Grant <justingrant99@me.com>
2022-02-13 17:43:14 -0800

    work

--- app/src/main/java/edu/uci/swe264p/retrofit/MovieListActivity.java
+++ app/src/main/java/edu/uci/swe264p/retrofit/MovieListActivity.java
@@ -25,38 +25,35 @@ public class MovieListActivity extends AppCompatActivity {
 
     static final String BASE_URL = "https://api.themoviedb.org/3/";
     final static String API_KEY = "856bdec268693b5f42fa58d720946353";
-
+    List<String> programs = new ArrayList<String>(
+            Arrays.asList(
+                    "Computer Science — Ph.D.",
+                    "Computer Science — M.S.",
+                    "Informatics — Ph.D.",
+                    "Informatics — M.S.",
+                    "Networked Systems — Ph.D.",
+                    "Networked Systems — Ph.D.",
+                    "Software Engineering — Ph.D.",
+                    "Software Engineering — M.S.",
+                    "Statistics — Ph.D.",
+                    "Statistics — M.S.",
+                    "Master of Computer Science (MCS)",
+                    "Master of Software Engineering (MSWE)",
+                    "Master of Human-Computer Interaction and Design (MHCID)",
+                    "Master of Embedded & Cyber-physical Systems (MECPS)"
+            )
+    );
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_list);
-        List<String> programs = new ArrayList<String>(
-                Arrays.asList(
-                        "Computer Science — Ph.D.",
-                        "Computer Science — M.S.",
-                        "Informatics — Ph.D.",
-                        "Informatics — M.S.",
-                        "Networked Systems — Ph.D.",
-                        "Networked Systems — Ph.D.",
-                        "Software Engineering — Ph.D.",
-                        "Software Engineering — M.S.",
-                        "Statistics — Ph.D.",
-                        "Statistics — M.S.",
-                        "Master of Computer Science (MCS)",
-                        "Master of Software Engineering (MSWE)",
-                        "Master of Human-Computer Interaction and Design (MHCID)",
-                        "Master of Embedded & Cyber-physical Systems (MECPS)"
-                )
-        );
+
 
 
         connect();
-        recyclerView = findViewById(R.id.rvMovieList);
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setAdapter(new ProgramListAdapter(programs));
+
     }
 
     private void connect() {
@@ -83,7 +80,12 @@ public class MovieListActivity extends AppCompatActivity {
 //                        response.body().getVoteAverage().toString(),
 //                        response.body().getOverview()
                 };
-                System.out.println(response.body().getMovieList().get(0).getTitle());
+                System.out.println(programs.add(response.body().getMovieList().get(0).getTitle()));
+                System.out.println("should be after");
+                recyclerView = findViewById(R.id.rvMovieList);
+                recyclerView.setHasFixedSize(true);
+                recyclerView.setLayoutManager(new LinearLayoutManager(MovieListActivity.this));
+                recyclerView.setAdapter(new ProgramListAdapter(programs));
 
             }
             @Override
